4519e682c2da19b3368c26eb964b6a87097461c7
Hyde46 <denis.heid@protonmail.com>
2020-02-05 11:50:11 +0100

    Add tests for clone trait of renderstructs

--- rusterize/src/test.rs
+++ rusterize/src/test.rs
@@ -200,6 +200,18 @@ fn test_ray_hit_triangle_not_in_triangle() {
     assert!(!t.intersects(&ray, &i_rec));
 }
 
+#[test]
+fn clone_vec3() {
+    let v1 = Vec3::new(0f32, 1f32, 2f32);
+    assert_eq!(v1.clone(), Vec3::new(0f32, 1f32, 2f32));
+}
+
+#[test]
+fn clone_ray() {
+    let r1 = Ray::new(Vec3::empty(), Vec3::empty(), 0_f32, 10000_f32);
+    assert_eq!(r1, r1.clone());
+}
+
 /*
 #[test]
 fn generate_orthographic_ray() {
